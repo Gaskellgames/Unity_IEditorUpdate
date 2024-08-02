418664adc070a0ff202a2e5bb500003dedaf6ed3
Gaskellgames <77272073+Gaskellgames@users.noreply.github.com>
2024-08-02 19:23:54 +0100

    Optimisations
    - Update EditorUpdateLoop static script

--- Gaskellgames/GgCore/Utilities/ProjectUtility/Editor/EditorUpdateLoop.cs
+++ Gaskellgames/GgCore/Utilities/ProjectUtility/Editor/EditorUpdateLoop.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 namespace Gaskellgames.EditorOnly
 {
@@ -28,28 +27,88 @@ namespace Gaskellgames.EditorOnly
         /// </summary>
         private static void HandleIEditorUpdateCallbacks()
         {
-            // for all existing components with IEditorUpdate unsubscribe them from the editor update loop
-            foreach (var editorUpdateMethod in editorUpdateList)
-            {
-                EditorApplication.update -= editorUpdateMethod.EditorUpdate;
-            }
-            editorUpdateList.Clear();
-            
             // playmode check
-            if (Application.isPlaying) { return; }
-            
-            // find all components with IEditorUpdate (including all inactive or disabled)
-            GameObject[] rootObjs = SceneManager.GetActiveScene().GetRootGameObjects();
-            foreach(GameObject root in rootObjs)
+            if (Application.isPlaying)
             {
-                editorUpdateList.AddRange(root.GetComponentsInChildren<IEditorUpdate>(true));
+                // unsubscribe all existing IEditorUpdate components from the editor update loop
+                if (0 < editorUpdateList.Count) { UnsubscribeFromEditorUpdateLoop(); }
+                return;
             }
 
-            // for all components with IEditorUpdate subscribe them to the editor update loop
-            foreach (var editorUpdateMethod in editorUpdateList)
+            // if list of all IEditorUpdate has changed ...
+            if (EditorUpdateListChanged())
             {
-                editorUpdateMethod.EditorUpdate();
+                // ... subscribe all IEditorUpdate components to editor update loop
+                SubscribeToEditorUpdateLoop();
+                VerboseLogs.Log($"Components using IEditorUpdate: {editorUpdateList.Count}");
             }
+        }
+
+        /// <summary>
+        /// Checks to see if the list of all IEditorUpdate has changed
+        /// </summary>
+        /// <returns>True if changed, False if not</returns>
+        private static bool EditorUpdateListChanged()
+        {
+            // get a list of all IEditorUpdate in the open scenes
+            List<IEditorUpdate> newEditorUpdateList = new List<IEditorUpdate>();
+            List<GameObject> rootObjects = SceneExtensions.GetAllRootGameObjects();
+            foreach(GameObject root in rootObjects)
+            {
+                IEditorUpdate[] iEditorUpdateArray = root.GetComponentsInChildren<IEditorUpdate>(true);
+                foreach (IEditorUpdate iEditorUpdate in iEditorUpdateArray)
+                {
+                    newEditorUpdateList.Add(iEditorUpdate);
+                }
+            }
+
+            // check to see if the list has been updated
+            foreach (IEditorUpdate iEditorUpdate in editorUpdateList)
+            {
+                if (newEditorUpdateList.Contains(iEditorUpdate)) { continue; }
+                UnsubscribeFromEditorUpdateLoop();
+                editorUpdateList = newEditorUpdateList;
+                return true;
+            }
+            foreach (IEditorUpdate iEditorUpdate in newEditorUpdateList)
+            {
+                if (editorUpdateList.Contains(iEditorUpdate)) { continue; }
+                UnsubscribeFromEditorUpdateLoop();
+                editorUpdateList = newEditorUpdateList;
+                return true;
+            }
+            
+            return false;
+        }
+
+        /// <summary>
+        /// Subscribe a list of components with IEditorUpdate to the editor update loop
+        /// </summary>
+        private static void SubscribeToEditorUpdateLoop()
+        {
+            for (var index = editorUpdateList.Count - 1; index >= 0; index--)
+            {
+                if (editorUpdateList[index] != null)
+                {
+                    EditorApplication.update += editorUpdateList[index].EditorUpdate;
+                }
+                else
+                {
+                    editorUpdateList.RemoveAt(index);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Unsubscribe a list of components with IEditorUpdate from the editor update loop
+        /// </summary>
+        private static void UnsubscribeFromEditorUpdateLoop()
+        {
+            foreach (IEditorUpdate iEditorUpdate in editorUpdateList)
+            {
+                EditorApplication.update -= iEditorUpdate.EditorUpdate;
+            }
+            editorUpdateList.Clear();
         }
 
     } // class end
